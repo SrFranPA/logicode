d6f78fd4608fb977d7baf737293b54085b475360
SrFranPA <srfranpa@gmail.com>
2025-10-28 11:02:02 -0500

    Crear usuario - Correccion

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.example.logicode"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = "27.0.12077973"
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
